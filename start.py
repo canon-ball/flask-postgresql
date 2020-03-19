6537a1509f767811f0f7fc4e7b31eb5c0d27eac6
canon-ball <canon_ball@mail.ru>
2020-03-19 21:22:51 +0300

    Done: Application:v1

--- start.py
+++ start.py
@@ -8,13 +8,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 class Day(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500), nullable=False)
     temperature = db.Column(db.String(20), nullable=False)
 
-db.create_all()
 
+db.create_all()
 
 
 @app.route('/', methods=['GET'])
@@ -32,6 +33,7 @@ def add_tmp():
 
     return redirect(url_for('main'))
 
+
 @app.route('/delete_tmp', methods=['POST'])
 def delete_tmp():
     text = request.form['text']
@@ -41,13 +43,16 @@ def delete_tmp():
 
     return redirect(url_for('main'))
 
+
 @app.route('/get_days')
 def show_days_temperature():
     return render_template('days.html', days=Day.query.all())
 
+
 @app.route('/delete', methods=['GET'])
 def delete_day():
     return render_template('delete_day.html', days=Day.query.all())
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
